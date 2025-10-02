c2253a7764356b2d9797cb4593df0078f623c585
jxrkjr <amonovjavohir2006@gmail.com>
2025-10-02 16:56:52 +0500

    deploy

--- root/settings.py
+++ root/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)9k*#_%wex&wdykm-ps)_xw(d!k53qkxe0novv)#=bzn-nqn^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
